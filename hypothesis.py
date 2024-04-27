cf8f4efc3f21275ba7ed10900d6a9d1ddd0e7d5b
tnt3o5 <thiendc3005@gmail.com>
2024-04-27 17:09:31 +0700

    post processing

---
+++ hypothesis.py
@@ -0,0 +1,15 @@
+"""
+### Third stage approach
+
+https://www.isixsigma.com/hypothesis-testing/hypothesis-testing-fear-no-more/
+
+Create a hypothesis test based on the cheat logs results
+- Cheat logs results should have features: time spend + action detected
+
+What we want to do is for suspected case, can he/she considered to break the exam regulations or not.
+At this stage, i proposed two hypo test that we can consider:
+1. Supposing time consuming on the screen on average >= 70% time of the test means trustable, 
+the real-time consuming for example is 65% can be considered violation ?
+
+2. If action 1 and action 2 shares the same time spent on, can we say they
+"""
